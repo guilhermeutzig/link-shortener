25d2ade1fca6889f608457e73ac874cfbc32dc18
Guilherme Utzig <guilhermelutzig@gmail.com>
2022-12-08 10:35:20 -0300

    Created slug endpoint

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../db/client";
+
+function errorMessage(res: NextApiResponse, message: string) {
+  res.statusCode = 404;
+  res.send(JSON.stringify({ message }));
+  return;
+}
+
+const getUrl = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {
+    query: { slug },
+  } = req;
+
+  if (!slug || typeof slug !== "string") {
+    return errorMessage(res, "Please use it with a slug!");
+  }
+
+  const data = await prisma.shortLink.findFirst({
+    where: {
+      slug: {
+        equals: slug,
+      },
+    },
+  });
+
+  if (!data) {
+    return errorMessage(res, "Slug not found");
+  }
+
+  return res.redirect(data.url);
+};
+
+export default getUrl;
